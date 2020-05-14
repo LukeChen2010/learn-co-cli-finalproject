44a038c6fc31e3b6ae7ca804f92839d60a350c71
Luke Chen <luke.chen.2010@gmail.com>
2020-05-13 22:37:48 -0500

    Save 5/13/2020

---
+++ RuneScapeItem.rb
@@ -0,0 +1,77 @@
+require "open-uri"
+require "net/http"
+require "json"
+require "date"
+
+class RuneScapeItem
+    attr_accessor :id
+    attr_accessor :item, :icon, :icon_large, :type, :typeIcon, :name, :description, :members, :current, :today, :day30, :day90, :day180
+    attr_accessor :daily_chart, :average_chart
+
+    def initialize(id)
+        @id = id
+        @daily_chart = {}
+        @average_chart = {}
+        self.get_item_details
+        self.get_price_chart
+    end
+
+    def get_item_details
+        url = "http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=#{@id}"
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        payload = JSON.parse(response.body)
+
+        @item = payload["item"]
+        @icon = @item["icon"]
+        @icon_large = @item["icon_large"]
+        @type = @item["type"]
+        @typeIcon = @item["typeIcon"]
+        @name = @item["name"]
+        @description = @item["description"]
+        @members = @item["members"]
+        @current = @item["current"]
+        @today = @item["today"]
+        @day30 = @item["day30"]
+        @day90 = @item["day90"]
+        @day180 = @item["day180"]
+    end
+
+    def get_price_chart
+        url = "http://services.runescape.com/m=itemdb_rs/api/graph/#{@id}.json"
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        payload = JSON.parse(response.body)
+
+        daily_hash = payload["daily"]
+        average_hash = payload["average"]
+
+        date = nil
+        
+        daily_hash.each do |key, value|
+             date = Time.at(key.to_i/1000)
+             @daily_chart[date.strftime("%B %d, %Y")] = value
+        end
+
+        average_hash.each do |key, value|
+            date = Time.at(key.to_i/1000)
+            @average_chart[date.strftime("%B %d, %Y")] = value
+       end
+    end
+
+    def get_max_daily_value
+        return @daily_chart.values.max
+    end
+
+    def get_max_daily_date
+        return  @daily_chart.key(get_max_daily)
+    end
+
+    def get_max_average_value
+        return @average_chart.values.max
+    end
+
+    def get_max_average_date
+        return  @average_chart.key(average_chart)
+    end
+end
---
+++ Test.rb
@@ -0,0 +1,3 @@
+require_relative "RuneScapeItem.rb"
+
+myitem = RuneScapeItem.new(21787)
