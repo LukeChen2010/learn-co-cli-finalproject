2dc79ab0a7a5290b81503390ee00b6bf249107b9
Luke Chen <luke.chen.2010@gmail.com>
2020-05-14 13:18:59 -0500

    Save 5/14/2020

--- RuneScapeItem.rb
+++ RuneScapeItem.rb
@@ -4,6 +4,7 @@ require "json"
 require "date"
 
 class RuneScapeItem
+
     attr_accessor :id
     attr_accessor :item, :icon, :icon_large, :type, :typeIcon, :name, :description, :members, :current, :today, :day30, :day90, :day180
     attr_accessor :daily_chart, :average_chart
@@ -50,28 +51,91 @@ class RuneScapeItem
         
         daily_hash.each do |key, value|
              date = Time.at(key.to_i/1000)
-             @daily_chart[date.strftime("%B %d, %Y")] = value
+             @daily_chart[date.strftime("%m/%d/%Y")] = value
         end
 
         average_hash.each do |key, value|
             date = Time.at(key.to_i/1000)
-            @average_chart[date.strftime("%B %d, %Y")] = value
+            @average_chart[date.strftime("%m/%d/%Y")] = value
        end
     end
 
-    def get_max_daily_value
-        return @daily_chart.values.max
+    def print_daily_chart
+        daily_chart.each do |key, value|
+            puts "#{key}: #{value}"
+        end
     end
 
-    def get_max_daily_date
-        return  @daily_chart.key(get_max_daily)
+    def print_average_chart
+        average_chart.each do |key, value|
+            puts "#{key}: #{value}"
+        end
     end
 
-    def get_max_average_value
-        return @average_chart.values.max
+end
+
+class Transaction
+
+    attr_accessor :item, :quantity, :price, :sell, :value
+
+    def initialize(item, quantity, price, sell)
+        @item = item
+        @quantity = quantity
+        @price = price
+        @sell = sell
     end
 
-    def get_max_average_date
-        return  @average_chart.key(average_chart)
+end
+
+class Portfolio
+
+    attr_accessor :items, :transactions 
+
+    def initialize
+        @items = {}
+        @transactions = []
     end
+
+    def add_item(id)
+        item = RuneScapeItem.new(id)
+        @items[item.name] = item
+    end
+
+    def delete_item(name)
+        @items.delete(name)
+    end
+
+    def add_transaction(item, quantity, price, sell)
+        transaction = Transaction.new(item, quantity, price, sell)
+        @transactions << transaction
+    end
+
+    def item_performance(name)
+        quantity_bought = 0
+        quantity_sold = 0 
+        price_bought = 0
+        price_sold = 0
+
+        item_buys = @transactions.find_all {|x| x.item.name == name && x.sell == false}
+        item_sales = @transactions.find_all {|x| x.item.name == name && x.sell == true}
+
+        item_buys.each do |x|
+            quantity_bought += x.quantity
+            price_bought += x.price
+        end
+
+        item_sales.each do |x|
+            quantity_sold += x.quantity
+            price_sold += x.price
+        end
+
+        puts "#{items[name].name}:"
+        puts "Bought: #{quantity_bought} for #{price_bought} (~#{(price_bought.to_f/quantity_bought.to_f).floor} each)"
+        puts "Sold: #{quantity_sold} for #{price_sold} (~#{(price_sold.to_f/quantity_sold.to_f).floor} each)"
+        puts "Return: #{(100*(price_sold.to_f-price_bought.to_f)/price_bought.to_f).round(2)}%"
+    end
+
+    def portfolio_performance
+    end
+
 end
--- Test.rb
+++ Test.rb
@@ -1,3 +1,21 @@
 require_relative "RuneScapeItem.rb"
 
-myitem = RuneScapeItem.new(21787)
+myport = Portfolio.new
+myport.add_item(21787)
+myport.add_item(21790)
+
+steadfast_boots = myport.items["Steadfast boots"]
+glaiven_boots = myport.items["Glaiven boots"]
+
+myport.add_transaction(steadfast_boots, 3, 4000000, false)
+myport.add_transaction(steadfast_boots, 7, 5000000, false)
+myport.add_transaction(steadfast_boots, 4, 7000000, false)
+myport.add_transaction(steadfast_boots, 9, 9000000, true)
+myport.add_transaction(steadfast_boots, 5, 7200000, true)
+
+myport.add_transaction(glaiven_boots, 9, 9000000, false)
+myport.add_transaction(glaiven_boots, 9, 9100000, true)
+
+myport.item_performance("Steadfast boots")
+puts
+myport.item_performance("Glaiven boots")
